54fa1f02ffdddc2745d2d457ee39e578ace9f531
Sabin Thapa <112601289+Sabinkthapa@users.noreply.github.com>
2023-10-26 20:07:13 -0300

    added code in model,starting routes codes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [{model:Category},{model:Tag}],
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async(req, res) => {
+  try {
+    const singleProduct = await Product.findByPk(req.params.id, {
+      include:[{model:Category},{model:Tag}],
+    });
+    if (!singleProduct) {
+      res.status(404).json({message: "no such product with that id"});
+      return;
+    }
+    res.status(200).json(singleProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
